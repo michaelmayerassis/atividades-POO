85e541e752821ba5ea59845915b2b3fab15393f2
Michael Mayer de Assis <48072358+michaelmayerassis@users.noreply.github.com>
2019-03-14 15:25:46 -0300

    Add files via upload

---
+++ Filme.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Filmes
+{
+    public class Filme
+    {
+        public string Nome;
+        public DateTime AnoLanc;
+        public string Descricao;
+        public decimal Nota;
+    }
+}
